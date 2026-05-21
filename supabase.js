1a84c2454efeca8a8cd53b6c3fa30d791048e730
Firiiken Belissa GC <41576931+Fapplication@users.noreply.github.com>
2026-05-21 04:03:21 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,6 @@
-import { createClient }
-from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
-const supabaseUrl = 'https://pkdzcvfbcbilflrdfagt.supabase.co/rest/v1/'
-const supabaseKey = 'YOUR_ANON_KEY'
+const supabaseUrl = "YOUR_SUPABASE_URL"
+const supabaseKey = "YOUR_SUPABASE_ANON_KEY"
 
-export const supabase =
-createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
