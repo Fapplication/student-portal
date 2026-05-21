178fbbe37c6a5f8fc2ccdb84f4a16e72ad0a0a9a
Firiiken Belissa GC <41576931+Fapplication@users.noreply.github.com>
2026-05-21 04:07:10 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,47 +1,24 @@
-import { supabase }
-from './supabase.js'
+import { supabase } from "./supabase.js"
 
-window.saveData = async function(){
+window.addStudent = async function () {
 
-const id =
-document.getElementById('id').value
+    const id = document.getElementById("id").value
+    const name = document.getElementById("name").value
+    const course = document.getElementById("course").value
+    const mark = document.getElementById("mark").value
 
-const name =
-document.getElementById('name').value
+    const { error } = await supabase
+        .from("students")
+        .insert([{
+            id,
+            name,
+            course,
+            mark
+        }])
 
-const course =
-document.getElementById('course').value
-
-const mark =
-document.getElementById('mark').value
-
-const email =
-document.getElementById('email').value
-
-const { error } =
-await supabase
-.from('students')
-.insert([{
-
-id,
-name,
-course,
-mark,
-email
-
-}])
-
-if(error){
-
-document.getElementById('message')
-.innerHTML = error.message
-
-}else{
-
-document.getElementById('message')
-.innerHTML =
-'Student Added Successfully'
-
-}
-
+    if (error) {
+        document.getElementById("msg").innerText = error.message
+    } else {
+        document.getElementById("msg").innerText = "Saved successfully!"
+    }
 }
