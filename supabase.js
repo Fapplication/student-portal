73ec428ca7626db7613edf389fe53ab8bf5d948e
Firiiken Belissa GC <41576931+Fapplication@users.noreply.github.com>
2026-05-21 04:30:03 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://pkdzcvfbcbilflrdfagt.supabase.co"
-const supabaseKey = "sb_publishable_zaImYNwzo_Zjbt5rknuO5A_h01sKFTy"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrZHpjdmZiY2JpbGZscmRmYWd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMjk3ODQsImV4cCI6MjA5NDkwNTc4NH0.KOH29kS9Dm6SD5f8x8TZOIUYDAzv3SSEw3Blq8fRGdc"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
